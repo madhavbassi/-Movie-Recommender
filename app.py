c6b82beff5f59714a4bdae8ebea0563b6b073907
Madhav bassi <madhavbassi1@GMAIL.COM>
2026-08-07 03:05:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -312,7 +312,7 @@ elif st.session_state.view == "details":
     with left:
         st.markdown("<div class='card'>", unsafe_allow_html=True)
         if data.get("poster_url"):
-            st.image(data["poster_url"], use_column_width=True)
+            st.image(data["poster_url"], width="stretch")
         else:
             st.write("🖼️ No poster")
         st.markdown("</div>", unsafe_allow_html=True)
